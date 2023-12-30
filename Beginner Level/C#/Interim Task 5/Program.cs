100f1c78739d3a59893b4427f5679b24af14e305
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2023-12-30 19:39:12 +0300

    Interim Task 5 Completed.

---
+++ Beginner Level/C#/Interim Task 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InterimTaskFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Good morning");
+            else if (time <= 18)
+                Console.WriteLine("Have a good day");
+            else
+                Console.WriteLine("Good night");
+
+            string result = time >= 6 && time < 11 ? "Good morning" : time <= 18 ? "Have a good day" : "Good night";
+            Console.WriteLine(result);
+        }
+    }
+}
