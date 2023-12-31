d861a14c276c92a7133b00388749234eae2aacd1
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2023-12-31 11:54:48 +0300

    Interim Task 9 Completed.

---
+++ Beginner Level/C#/Interim Task 9/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace InterimTaskNine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = new string[5];
+
+            string[] animals = {"Cat", "Dog", "Bird", "Monkey"};
+
+            int[] array1;
+            array1 = new int[5];
+
+            colors[0] = "Blue";
+            array1[3] = 10;
+
+            Console.WriteLine(animals[1]);
+            Console.WriteLine(array1[3]);
+            Console.WriteLine(colors[0]);
+
+            Console.WriteLine("Please enter the number of elements of the array:");
+            int arrLength = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[arrLength];
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.Write("Please enter {0}. element: ", i+1);
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int total = 0;
+            foreach (var number in numbers)
+            {
+                total += number;                
+            }
+
+            Console.WriteLine("Average: " + total / arrLength);
+        }
+    }
+}
