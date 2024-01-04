e4ef7ddd7a5c1bc2a053819f7f6178a6fa867cf7
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-01-04 22:31:57 +0300

    Task 2 Section 3 Completed.

--- Beginner Level/C#/Task 2/Third/Program.cs
+++ Beginner Level/C#/Task 2/Third/Program.cs
@@ -1,9 +1,48 @@
-﻿namespace Third;
+﻿using System.Collections;
 
-class Program
+namespace Third
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DESCRIPTION:\n");
+            Console.WriteLine("Write the programme that stores the vowels in the sentence entered from the keyboard in an array and sorts the elements of the array.\n");
+
+            Console.WriteLine("INFO: You must enter a sentence.");
+
+            ArrayList vowels = new ArrayList();
+            
+            string sentence = "";
+
+            do
+            {
+                Console.Write("Write a sentence: ");
+                sentence = Console.ReadLine();
+            }
+            while(sentence == "");
+
+            if(sentence != null && sentence != "")
+            {
+                foreach (var letter in sentence)
+                {
+                    if(letter == 'a' 
+                    || letter == 'e'  
+                    || letter == 'i' 
+                    || letter == 'o' 
+                    || letter == 'u')
+                        vowels.Add(letter);
+                }
+            }
+
+            vowels.Sort();
+
+            foreach (var item in vowels)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
     }
 }
