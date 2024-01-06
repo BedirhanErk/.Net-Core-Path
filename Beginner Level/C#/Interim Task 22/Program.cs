06a0e201ea68bb4e6690069be8f3fef71289f9e4
Bedirhan Erkılıç <erkilicbedirhan@gmail.com>
2024-01-06 14:49:11 +0300

    Interim Task 22 Completed.

---
+++ Beginner Level/C#/Interim Task 22/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Interim_TaskTwentyTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Sunday);
+            Console.WriteLine((int)Days.Saturday);
+
+            int temperature = 32;
+
+            if(temperature <= (int)Weather.Snowy)
+                Console.WriteLine("It is Snowy.");
+            else if(temperature > (int)Weather.Snowy && temperature <= (int)Weather.Stormy)
+                Console.WriteLine("It is Stormy."); 
+            else if(temperature > (int)Weather.Stormy && temperature <= (int)Weather.Rainy)
+                Console.WriteLine("It is Rainy.");   
+            else if(temperature >= (int)Weather.Sunny)
+                Console.WriteLine("It is Sunny."); 
+            else    
+                Console.WriteLine("Weather forecast is unknown");                  
+        }
+    }
+
+    enum Days
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday = 23,
+        Saturday,
+        Sunday
+    }
+
+    enum Weather
+    {
+        Snowy = 5,
+        Stormy = 8,
+        Rainy = 15,                        
+        Sunny = 30
+    }
+}
